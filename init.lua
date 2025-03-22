1bde30825097fa30b67f31712d43d6b292766656
Marco Túlio Souza <marcotuliorps@gmail.com>
2025-03-22 20:35:47 -0300

    break init.lua into some files ( set.lua and remap.lua)

--- init.lua
+++ init.lua
@@ -14,87 +14,10 @@ Kickstart Guide:
 
 If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
 --]]
-
--- Set <space> as the leader key
--- See `:help mapleader`
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
-
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
-
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.opt.showmode = true
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
-
--- Enable break indent
-vim.opt.breakindent = true
-
--- Save undo history
-vim.opt.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
-
--- Decrease update time
-vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
-vim.opt.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.opt.confirm = true
-
-require 'marco/remap'
-
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
+--
+--
+--
+require 'marco/set' -- this must be the first line. is here that is defined the leader key
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
@@ -127,6 +50,8 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+require 'marco/remap'
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
--- lua/marco/remap.lua
+++ lua/marco/remap.lua
@@ -1,5 +1,16 @@
 vim.keymap.set('v', 'E', '5e', { desc = 'select next 5 words' })
 vim.keymap.set('n', '<leader>u', ":UndotreeToggle<CR> | C-w h' ", { desc = 'undotree config' })
-vim.keymap.set('n', '<leader>bd', ":bdelete<CR> | C-w h' ", { desc = '[B]uffer [D]elete' })
-vim.keymap.set('n', '<leader>fbd', ":bdelete!<CR> | C-w h' ", { desc = 'force [B]uffer delete' })
-print 'marco file loaded'
+
+vim.keymap.set('n', '<leader><F4>', ':bdelete!<CR>', { desc = '[B]uffer [D]elete' })
+
+vim.keymap.set('n', '<leader><F12>', ':Telescope lsp_document_symbols<CR>', { desc = 'Document symbols' })
+vim.keymap.set('n', '<leader><F11>', ':Telescope lsp_workspace_symbols<CR>', { desc = 'Workspace symbols' })
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'move' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'mov' })
+vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Keep cursor em J' })
+
+-- I think I do not need this ctrl+c mapping
+vim.keymap.set('n', 'C-c', '<ESQ>')
+vim.keymap.set('i', 'C-c', '<ESQ>')
+vim.keymap.set('v', 'C-c', '<ESQ>')
