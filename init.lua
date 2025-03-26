1104d366030935c04f2c977c5b5e9c5d2c863e06
Marco Túlio Souza <marco.souza@inter.co>
2025-03-26 09:26:55 -0300

    init.lua treesitter ensure install xml and java

--- init.lua
+++ init.lua
@@ -18,6 +18,7 @@ If you experience any errors while trying to install kickstart, run `:checkhealt
 --
 --
 require 'marco/set' -- this must be the first line. is here that is defined the leader key
+-- require 'marco/java'
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
@@ -820,7 +821,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'xml', 'java' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
